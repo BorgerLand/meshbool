2826ce4c8638356d832e9aeb5e50898aa2e30012
luisfonsivevo <jerbear3.14159@gmail.com>
2026-05-31 22:21:04 +0200

    ai slop cleanup

--- build.rs
+++ build.rs
@@ -8,23 +8,20 @@ fn main() {
 	#[cfg(feature = "test")]
 	{
 		build_rs::output::rerun_if_changed("Cargo.toml");
-		let crate_dir = build_rs::input::cargo_manifest_dir();
-		let out_dir = build_rs::input::out_dir();
-
 		build_rs::output::rerun_if_changed("test/test.zng");
 
-		let generated_cpp = path::PathBuf::from("test/generated/meshbool/");
-		let _ = fs::create_dir_all(&generated_cpp);
-		let rs_file = out_dir.join("generated.rs");
-		let h_file = generated_cpp.join("meshbool.h");
+		let crate_dir = build_rs::input::cargo_manifest_dir();
+		let generated_dir = path::PathBuf::from("test/generated");
+		let out_dir = build_rs::input::out_dir();
+
+		fs::create_dir_all(&generated_dir).unwrap();
+		let rs_file = out_dir.join("test.rs");
 
 		Zngur::from_zng_file(crate_dir.join("test/test.zng"))
-			.with_cpp_file(generated_cpp.join("generated.cpp"))
-			.with_h_file(h_file)
+			.with_cpp_file(generated_dir.join("test.cpp"))
+			.with_h_file(generated_dir.join("test.h"))
 			.with_rs_file(rs_file.clone())
+			.with_zng_header(generated_dir.join("zngur.h"))
 			.generate();
-		let s = fs::read_to_string(&rs_file).expect("File should exist");
-		let new = s.replace("#[no_mangle]", "#[unsafe(no_mangle)]");
-		fs::write(rs_file, new).expect("Failed to write file");
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -33,34 +33,16 @@ mod utils;
 mod vec;
 
 #[cfg(feature = "test")]
-mod zngur_stubs;
+mod test;
 
 #[cfg(feature = "test")]
-use zngur_stubs::{
-    Vector3i32Coords as _, Vector4f64Coords as _, Vector4i32Coords as _,
-    Point2f64Coords as _, Point3f64Coords as _,
+use test::{
+	Point2f64Coords as _, Point3f64Coords as _, Vector3i32Coords as _, Vector4f64Coords as _,
+	Vector4i32Coords as _,
 };
 
 #[cfg(feature = "test")]
-include!(concat!(env!("OUT_DIR"), "/generated.rs"));
-
-#[test]
-fn test() {
-	use nalgebra::Vector3;
-
-	//just make sure it don't crash at the sight of the simplest shapes
-	let cube1 = MeshBool::cube(Vector3::new(1.0, 1.0, 1.0), true);
-	let cube2 = MeshBool::cube(Vector3::new(1.0, 1.0, 1.0), false);
-
-	let union = &cube1 + &cube2;
-	println!("{:?}", union.get_mesh_gl_32(0));
-
-	let difference = &cube1 - &cube2;
-	println!("{:?}", difference.get_mesh_gl_32(0));
-
-	let intersection = &cube1 ^ &cube2;
-	println!("{:?}", intersection.get_mesh_gl_32(0));
-}
+include!(concat!(env!("OUT_DIR"), "/test.rs"));
 
 fn halfspace(b_box: AABB, mut normal: Vector3<f64>, origin_offset: f64) -> MeshBool {
 	normal.normalize_mut();
